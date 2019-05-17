1a7c6be08c5488642ba209cf81081d07dd9ae4db
ester100 <riveraperezmariaester@gmail.com>
2019-05-17 11:15:00 -0600

    ester

--- www/js/app.js
+++ www/js/app.js
@@ -81,7 +81,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
-  });
+  })
+
+
+  .state('tab.juego', {
+    url: '/juego',
+    views: {
+      'tab-juego': {
+        templateUrl: 'templates/tab-juego.html',
+        controller: 'JuegoCtrl'
+      }
+    }
+  })
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
